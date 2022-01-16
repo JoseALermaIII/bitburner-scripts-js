5398f82da9b65220555298383b286de56e675f12
Jose A Lerma <jose@JoseALerma.com>
2022-01-16 14:41:52 -0600

    Refactored home scripts

--- src/main.js
+++ src/main.js
@@ -19,7 +19,11 @@ export async function main(ns) {
 	await ns.write(logFile, "Start main.js\n", logMode);
 	
 	await startUp.main(ns, "comptek"); // Start purchased and hacked servers
-	ns.run("earlyHackTemplate.js", 6400, "the-hub"); // Start home server
+	// Start home server. Current RAM = 16.38 TB
+	ns.run("earlyHackTemplate.js", 256, "the-hub");
+	ns.run("earlyHackTemplate.js", 1600, "rothman-uni");
+	ns.run("earlyHackTemplate.js", 1024, "johnson-ortho");
+	ns.run("earlyHackTemplate.js", 2048, "catalyst");
 	
 	await ns.write(logFile, "End main.js\n", "a");
 	ns.tprint("main.js Done.");
