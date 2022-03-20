804e74b396728e6a48055f3fe9aa195f9b772ecb
Jose A Lerma <jose@JoseALerma.com>
2022-03-19 22:44:14 -0500

    Refactor targets for new level and RAM

--- src/main.js
+++ src/main.js
@@ -18,12 +18,12 @@ const logMode = "w";
 export async function main(ns) {
 	await ns.write(logFile, "Start main.js\n", logMode);
 	
-	await startUp.main(ns, "comptek"); // Start purchased and hacked servers
-	// Start home server. Current RAM = 16.38 TB
+	await startUp.main(ns, "catalyst"); // Start purchased and hacked servers
+	// Start home server. Current RAM = 32.77 TB
 	ns.run("earlyHackTemplate.js", 256, "the-hub");
+	ns.run("earlyHackTemplate.js", 512, "comptek");
 	ns.run("earlyHackTemplate.js", 1600, "rothman-uni");
-	ns.run("earlyHackTemplate.js", 1024, "johnson-ortho");
-	ns.run("earlyHackTemplate.js", 2048, "catalyst");
+	ns.run("earlyHackTemplate.js", 6144, "netlink");
 	
 	await ns.write(logFile, "End main.js\n", "a");
 	ns.tprint("main.js Done.");
